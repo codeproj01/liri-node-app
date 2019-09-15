406c9b0cb70898b7f04582d1786ce3b2049c93fc
codeproj01 <cliftonmpls@comcast.net>
2019-09-15 00:27:56 -0500

    Deployment

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # liri-node-app
+This assignment is to make LIRI, a _Language_ Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+
+It's just like iPhone's Siri, but without using spoken language. LIRI is a command line node app.
+LIRI is used to request data from several APIs based on user input by searching the APIs databases for the information and returns output
+in a JSON format.
+
+LIRI's environment is setup by importing NPM modules and packages:
+dotenv: dotenv is a zero-dependency module that loads environment variables from a .env file into process.env. 
+Command Line: 'npm install dotenv'
+Request: - Request is designed to be the simplest way possible to make http calls. It supports HTTPS and follows redirects by default.
+Command Line: 'npm install request'
+Moment: - A lightweight JavaScript date library for parsing, validating, manipulating, and formatting dates.
+Command Line: 'npm install moment'
+Fs: - a built in node package
+Run command "npm install" in Terminal to pul in NPM modules
+Run command "npm install axios" in Terminal for AXIOS.
+Run command "node liri.js" to execute the app and pass it some arguments.
+
+The LIRI App is organized using several functions and a switch statement. LIRI includes logging support both to the console and an output file - log.txt.
+It uses the npm node module, 'simple-node-logger', for it's logging solution.
+"npm install simple-node-logger --save"
+LIRI will search Spotify for songs, Bands in Town for concerts, and OMDB for movies.
+spotify-this-song <song title> will display information about the song such as the artist, album name and a preview link
+concert-this <Artist name> Will display upcoming concerts by the Artist.
+movie-this <movie title> will information about the movie such as Rotten Tomatoes rating, year, plot, and actors
+do-what-it-says takes the text from random.txt and executes it
+
+Built With
+Node.js
+[Node-Spotify-API](https://www.npmjs.com/package/node-spotify-api)
+[AXIOS](https://www.npmjs.com/package/axios)
+[OMDB API](http://www.omdbapi.com) 
+[Bands In Town API](http://www.artists.bandsintown.com/bandsintown-api)
+[Moment](https://www.npmjs.com/package/moment)
+[DotEnv](https://www.npmjs.com/package/dotenv)
+
+Developer: Clifton Nwokeuku
+
+Instructions:
+To run LIRI:
+
---
+++ liri.js
@@ -0,0 +1,177 @@
+//Read and set any environment variables with the NPM and dotenv package:
+
+require("dotenv").config();
+
+const fs = require("fs");
+const moment = require("moment");
+
+const axios = require("axios");
+const Spotify = require("node-spotify-api");
+
+const keys = require("./keys.js");
+//const inquirer = require('inquirer');
+
+//Output file for logs.
+const filename = './log.txt';
+
+//NPM module used for logging solution.
+const log = require('simple-node-logger').createSimpleFileLogger( filename );
+
+//All log information printed to log.txt.
+log.setLevel('all');
+
+var userInput = process.argv;
+var inputChoice = process.argv[2];
+
+//LIRI will search Spotify for songs, Bands in Town for concerts, and OMDB for movies.
+
+//This function will search the Bands in Town Artist Events API (`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"`) 
+//for an artist and render the following information about each event to the terminal:
+
+function bandsInTown(){
+    var artist = "";
+    for (var i = 3; i < userInput.length; i++){
+        if (i > 3 && i < userInput.length){
+            artist = artist + "+" + userInput[i];
+        }
+        else{
+            artist += userInput[i];
+        }
+    }
+    if (userInput.length < 4) {
+        artist = "Cardi B";
+    }
+
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    
+    logOutput(queryURL); 
+
+    axios.get(queryURL).then(
+        function(bandResponse){
+            logOutput("Venue: " + bandResponse.data[0].venue.name);
+            logOutput("City: " + bandResponse.data[0].venue.city);
+            logOutput(moment(bandResponse.data[0].datetime).format("MM/DD/YYYY"));
+        })
+    };
+
+
+//This function will show the following information about the song in your terminal/bash window Artist(s), The song's name, A preview link of the song from Spotify, 
+//The album that the song is from]
+
+ function songChoice(){
+    var songName = "";
+    for (var i = 3; i < userInput.length; i++){
+        if (i > 3 && i < userInput.length){
+            songName = songName + "+" + userInput[i];
+        }
+        else{
+            songName += userInput[i];
+        }
+    }
+    if (userInput.length < 4) {
+        songName = "Ace of Base The Sign";
+    }
+    
+    var spotify = new Spotify(keys.spotify)
+
+    spotify.request('https://api.spotify.com/v1/search?q=track:' + songName + '&type=track', function(error, songResponse) {
+        if (error){
+            return logOutput(error);
+        }
+        logOutput("Artist: " + songResponse.tracks.items[0].artists[0].name);
+        logOutput("Song: " + songResponse.tracks.items[0].name);
+        logOutput("URL: " + songResponse.tracks.items[0].preview_url);
+        logOutput("Album: " + songResponse.tracks.items[0].album.name);
+    });
+};
+
+//This function will output the following information to your terminal/bash window:(Title of the movie, Year the movie came out, IMDB Rating of the movie, 
+//Rotten Tomatoes Rating of the movie., Country where the movie was produced. Language of the movie, Plot of the movie, Actors in the movie.)
+
+function movieChoice(){
+ 
+    var movieName = "";
+
+    for (var i = 3; i < userInput.length; i++){
+        if (i > 3 && i < userInput.length){
+            movieName = movieName + "+" + userInput[i];
+        }
+        else{
+            movieName += userInput[i];
+        }
+    };
+
+    if (userInput.length < 4) {
+        movieName = "Mr. Nobody";
+    }
+    
+    var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=f3f1608e";
+
+
+    axios.get(queryURL).then(
+        function(movieResponse){
+            logOutput("Title: " + movieResponse.data.Title);
+            logOutput("Year: " + movieResponse.data.Year);
+            logOutput("Rated: " + movieResponse.data.imdbRating);
+            logOutput("Country: " + movieResponse.data.Country);
+            logOutput("Language: " + movieResponse.data.Language);
+            logOutput("Plot: " + movieResponse.data.Plot);
+            logOutput("Actors: " + movieResponse.data.Actors);
+            logOutput("Rotten Tomatoes: " + movieResponse.data.Ratings[1].Value);
+        });
+    };
+
+//Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+//It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
+
+function doWhatItSays() {
+
+    fs.readFile("random.txt", "utf8", function(error, data) {
+      if (error) {
+        return logOutput(error);
+      }
+        var output = data.split(",");
+        for (var i = 0; i < output.length; i++) {
+            logOutput(output[i]);
+        }
+      });
+};
+
+//Setting up the Switch Statement based on the user input to complete the API request. .
+//Switch Statement with logging information.
+
+switch (inputChoice){
+    case "concert-this":
+        //logs.log(fs, `\n${starSplitter}\nCOMMAND ISSUED: ${itemOBJ.query}`);
+        logOutput("=====================================================================");
+        logOutput("user command: " + userInput[2] + "   user arguments: " + userInput[3]);
+        bandsInTown();
+        break;
+    
+    case "spotify-this-song":
+        logOutput("=====================================================================");
+        logOutput("user command: " + userInput[2] + "   user arguments: " + userInput[3]);
+        songChoice();
+        break;
+    
+    case "movie-this":
+        logOutput("=====================================================================");
+        logOutput("user command: " + userInput[2] + "   user arguments: " + userInput[3]);
+        movieChoice();
+        break;
+
+    case "do-what-it-says":
+        logOutput("=====================================================================");
+        logOutput("user command: " + userInput[2] + "   user arguments: " + userInput[3]);
+        doWhatItSays();
+        break;
+}
+
+//Logs data to the terminal and output to a text file called log.txt.
+
+function logOutput(logText) {
+	log.info(logText);
+    let str = "---------------------";
+    //Suppresses console logging users commands to terminal output, since users can see what they typed.
+    console.log(logText.replace(/^user command.*$\n?/gm, ''));
+}
