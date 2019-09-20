6df420fa86fa5a4de1f439b631b4271ff799dc48
codeproj01 <cliftonmpls@comcast.net>
2019-09-19 21:31:18 -0500

    updated code

--- liri.js
+++ liri.js
@@ -1,4 +1,4 @@
-//Read and set any environment variables with the NPM and dotenv package:
+//Global environment variables and importing NPM and dotenv packages:
 
 require("dotenv").config();
 
@@ -14,7 +14,7 @@ const keys = require("./keys.js");
 //Output file for logs.
 const filename = './log.txt';
 
-//NPM module used for logging solution.
+//NPM module used for logging solution. simple-node-logger - npm
 const log = require('simple-node-logger').createSimpleFileLogger( filename );
 
 //All log information printed to log.txt.
@@ -25,19 +25,54 @@ let inputChoice = process.argv[2];
 
 //LIRI will search Spotify for songs, Bands in Town for concerts, and OMDB for movies.
 
-//This function will search the Bands in Town Artist Events API for a Band/Artist and render the following information about each event to the terminal:
+//Start LIRI BOT
+
+//Setting up the Switch Statement based on the user input to complete the API request.
+//Switch Statement with logging information from user input.
+
+switch (inputChoice){
+    case "concert-this":
+        logOutput("=====================================================================");
+        logOutput("user command: " + userInput[2] + "   user arguments: " + process.argv.slice(3).join(" "));
+        bandsInTown();
+        break;
+    
+    case "spotify-this-song":
+        logOutput("=====================================================================");
+        logOutput("user command: " + userInput[2] + "   user arguments: " + process.argv.slice(3).join(" "));
+        songChoice();
+        break;
+    
+    case "movie-this":
+        logOutput("=====================================================================");
+        logOutput("user command: " + userInput[2] + "   user arguments: " + process.argv.slice(3).join(" "));
+        movieChoice(userInput);
+        break;
+
+    case "do-what-it-says":
+        logOutput("=====================================================================");
+        logOutput("user command: " + userInput[2] + "   user arguments: " + process.argv.slice(3).join(" "));
+        doWhatItSays();
+        break;
+}
+
+//This function will search the Bands in Town Artist Events API for a Band/Artist and render the  specific information about each event to the terminal:
 
 function bandsInTown(){
+    
+    let myArray = userInput;
     let artist = "";
-    for (let i = 3; i < userInput.length; i++){
-        if (i > 3 && i < userInput.length){
-            artist = artist + "+" + userInput[i];
+
+    for (let i = 3; i < myArray.length; i++){
+        if (i > 3 && i < myArray.length){
+            artist = artist + "+" + myArray[i];
         }
         else{
-            artist += userInput[i];
+           artist += myArray[i];
         }
     }
-    if (userInput.length < 4) {
+    
+    if (myArray.length < 4) {
         artist = "Cardi B";
     }
 
@@ -46,10 +81,10 @@ function bandsInTown(){
     logOutput(queryURL); 
 
     axios.get(queryURL).then(
-        function(bandResponse){
-            logOutput("Venue: " + bandResponse.data[0].venue.name);
-            logOutput("City: " + bandResponse.data[0].venue.city);
-            logOutput(moment(bandResponse.data[0].datetime).format("MM/DD/YYYY"));
+        function(result){
+            logOutput("Venue: " + result.data[0].venue.name);
+            logOutput("City: " + result.data[0].venue.city);
+            logOutput(moment(result.data[0].datetime).format("MM/DD/YYYY"));
         })
         .catch(function (error) {
             console.log("NOTHING FOUND");
@@ -61,30 +96,32 @@ function bandsInTown(){
 //and the album that the song is from.
 
  function songChoice(){
+
+    let myArray = userInput;
     let songName = "";
-    for (let i = 3; i < userInput.length; i++){
-        if (i > 3 && i < userInput.length){
-            songName = songName + "+" + userInput[i];
+    
+    for (let i = 3; i < myArray.length; i++){
+        if (i > 3 && i < myArray.length){
+            songName = songName + "+" + myArray[i];
         }
         else{
-            songName += userInput[i];
+            songName += myArray[i];
         }
     }
-    if (userInput.length < 4) {
+    if (myArray.length < 4) {
         songName = "Ace of Base The Sign";
     }
     
-    let spotify = new Spotify(keys.spotify)
+    const spotify = new Spotify(keys.spotify)
 
-    spotify.request('https://api.spotify.com/v1/search?q=track:' + songName + '&type=track', function(error, songResponse) {
-        if (error){
-           // return logOutput(error);
-            return logOutput("NOTHING FOUND");
+    spotify.search({ type: 'track', query: songName  }, function(err, result) {
+        if (err) {
+        return logOutput("NOTHING FOUND");
         }
-        logOutput("Artist: " + songResponse.tracks.items[0].artists[0].name);
-        logOutput("Song: " + songResponse.tracks.items[0].name);
-        logOutput("URL: " + songResponse.tracks.items[0].preview_url);
-        logOutput("Album: " + songResponse.tracks.items[0].album.name);
+        logOutput("Artist: " + result.tracks.items[0].artists[0].name);
+        logOutput("Song: " + result.tracks.items[0].name);
+        logOutput("Preview Link: " + result.tracks.items[0].preview_url);
+        logOutput("Album: " + result.tracks.items[0].album.name);
     });
 };
 
@@ -92,35 +129,37 @@ function bandsInTown(){
 //Rotten Tomatoes Rating of the movie, Country where the movie was produced. Language of the movie, Plot of the movie, Actors in the movie.
 
 function movieChoice(){
- 
+
+    let myArray = userInput;
     let movieName = "";
 
-    for (let i = 3; i < userInput.length; i++){
-        if (i > 3 && i < userInput.length){
-            movieName = movieName + "+" + userInput[i];
+    for (let i = 3; i < myArray.length; i++){
+        if (i > 3 && i < myArray.length){
+            movieName = movieName + "+" + myArray[i];
         }
         else{
-            movieName += userInput[i];
+            movieName += myArray[i];
         }
-    };
-
-    if (userInput.length < 4) {
+    }
+    
+    if (myArray.length < 4) {
         movieName = "Mr. Nobody";
     }
     
     let queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=f3f1608e";
 
-
+    //send requests using the axios package
     axios.get(queryURL).then(
-        function(movieResponse){
-            logOutput("Title: " + movieResponse.data.Title);
-            logOutput("Year: " + movieResponse.data.Year);
-            logOutput("Rated: " + movieResponse.data.imdbRating);
-            logOutput("Country: " + movieResponse.data.Country);
-            logOutput("Language: " + movieResponse.data.Language);
-            logOutput("Plot: " + movieResponse.data.Plot);
-            logOutput("Actors: " + movieResponse.data.Actors);
-            logOutput("Rotten Tomatoes: " + movieResponse.data.Ratings[1].Value);
+        function(result){
+            logOutput("Title: " + result.data.Title);
+            logOutput("Year: " + result.data.Year);
+            logOutput("Rated: " + result.data.imdbRating);
+            logOutput("Rotten Tomatoes: " + result.data.Ratings[1].Value);
+            logOutput("Country: " + result.data.Country);
+            logOutput("Language: " + result.data.Language);
+            logOutput("Plot: " + result.data.Plot);
+            logOutput("Actors: " + result.data.Actors);
+            
         })
         .catch(function (error) {
             console.log("NOTHING FOUND");
@@ -143,34 +182,6 @@ function doWhatItSays() {
       });
 };
 
-//Setting up the Switch Statement based on the user input to complete the API request.
-//Switch Statement with logging information from user input.
-
-switch (inputChoice){
-    case "concert-this":
-        logOutput("=====================================================================");
-        logOutput("user command: " + userInput[2] + "   user arguments: " + process.argv.slice(3).join(" "));
-        bandsInTown();
-        break;
-    
-    case "spotify-this-song":
-        logOutput("=====================================================================");
-        logOutput("user command: " + userInput[2] + "   user arguments: " + process.argv.slice(3).join(" "));
-        songChoice();
-        break;
-    
-    case "movie-this":
-        logOutput("=====================================================================");
-        logOutput("user command: " + userInput[2] + "   user arguments: " + process.argv.slice(3).join(" "));
-        movieChoice();
-        break;
-
-    case "do-what-it-says":
-        logOutput("=====================================================================");
-        logOutput("user command: " + userInput[2] + "   user arguments: " + process.argv.slice(3).join(" "));
-        doWhatItSays();
-        break;
-}
 
 //Logs data to the terminal and output to a text file called log.txt.
 
